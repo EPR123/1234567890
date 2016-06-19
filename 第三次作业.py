dd30467e7b01c3db97879df04856fa963739c172
EPR123 <910238253@qq.com>
2016-06-19 13:25:02 +0800

    Update 第三次作业.py

--- 第三次作业.py
+++ 第三次作业.py
@@ -1,9 +1,9 @@
 def my_name():
-    print"  #           #           #                       #           #            #          #
-    print"   #         # #         #                        #       #       #        #          #
-    print"    #       #   #       #                         #      #         #       #          #
-    print"     #     #     #     #                          #      #         #       #          #
-    print"      #   #       #   #                    #      #      #         #       #          #
-    print"       # #         # #                      #     #      #         #         #       # 
-    print"        #           #                           #        #         #             #
+    print"  #           #           #                       #           #            #          #"
+    print"   #         # #         #                        #       #       #        #          #"
+    print"    #       #   #       #                         #      #         #       #          #"
+    print"     #     #     #     #                          #      #         #       #          #"
+    print"      #   #       #   #                    #      #      #         #       #          #"
+    print"       # #         # #                      #     #      #         #         #       #"
+    print"        #           #                           #        #         #             #"
 print my_name()
